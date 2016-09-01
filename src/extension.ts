6ab2769c8a1ae5afd40ed20bc61b8c5d067945f4
tamas.kiss <kisstkondoros@gmail.com>
2016-09-01 23:45:43 +0200

    Add configuration options for reference counting and localization
    This fixes #3

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,41 @@ import * as path from 'path';
 import {CodeLensProvider, TextDocument, CancellationToken, CodeLens, Range, Command, Location, commands} from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+    class TypeLensConfiguration {
+        public exludeself: boolean = true;
+        public singular: string = "{0} reference";
+        public plural: string = "{0} references";
+        public noreferences: string = "no references found";
+    }
+
+    class AppConfiguration {
+        private cachedSettings: TypeLensConfiguration;
+        constructor() {
+            vscode.workspace.onDidChangeConfiguration(e => {
+                this.cachedSettings = null;
+            });
+        }
+
+        get extensionName() {
+            return 'typelens';
+        }
+
+        public typeLensEnabled: boolean = true;
+
+        get settings(): TypeLensConfiguration {
+            if (!this.cachedSettings) {
+                var settings = vscode.workspace.getConfiguration(this.extensionName);
+                this.cachedSettings = new TypeLensConfiguration();
+                for (var propertyName in this.cachedSettings) {
+                    if (settings.has(propertyName)) {
+                        this.cachedSettings[propertyName] = settings.get(propertyName);
+                    }
+                }
+            }
+            return this.cachedSettings;
+        }
+    }
+
     class MethodReferenceLens extends CodeLens {
         uri: vscode.Uri;
 
@@ -17,6 +52,10 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
     class TSCodeLensProvider implements CodeLensProvider {
+        private config: AppConfiguration;
+        constructor() {
+            this.config = new AppConfiguration();
+        }
 
         private getSourceFile(document: vscode.TextDocument) {
             return ts.createSourceFile(document.fileName, document.getText(), ts.ScriptTarget.Latest, true);
@@ -49,15 +88,39 @@ export function activate(context: vscode.ExtensionContext) {
             });
         }
         resolveCodeLens(codeLens: CodeLens, token: CancellationToken): CodeLens | Thenable<CodeLens> {
-
             if (codeLens instanceof MethodReferenceLens) {
-                return commands.executeCommand<Location[]>('vscode.executeReferenceProvider', codeLens.uri, codeLens.range.start).then(locations => {
 
-                    return new CodeLens(codeLens.range, {
-                        command: 'editor.action.showReferences',
-                        title: locations.length + ' references',
-                        arguments: [codeLens.uri, codeLens.range.start, locations],
-                    });
+                return commands.executeCommand<Location[]>('vscode.executeReferenceProvider', codeLens.uri, codeLens.range.start).then(locations => {
+                    var settings = this.config.settings;
+                    var filteredLocations = locations;
+                    if (settings.exludeself) {
+                        filteredLocations = locations.filter(location => !location.range.isEqual(codeLens.range));
+                    }
+
+                    var message;
+                    var amount = filteredLocations.length;
+                    if (amount == 0) {
+                        message = settings.noreferences;
+                    } else if (amount == 1) {
+                        message = settings.singular;
+                    } else {
+                        message = settings.plural;
+                    }
+                    message = message.replace('{0}', amount + "");
+
+                    if (amount > 0) {
+                        return new CodeLens(codeLens.range, {
+                            command: 'editor.action.showReferences',
+                            title: message,
+                            arguments: [codeLens.uri, codeLens.range.start, filteredLocations],
+                        });
+                    } else {
+                        return new CodeLens(codeLens.range, {
+                            command: "editor.action.findReferences",
+                            title: message,
+                            arguments: [codeLens.uri, codeLens.range.start]
+                        });
+                    }
                 });
             }
         }
